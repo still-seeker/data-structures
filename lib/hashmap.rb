7800fbfa1f7f310c7f3ccdedf5c6eba545e73293
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-04-20 07:59:38 +0300

    Create a HashMap class.

---
+++ lib/hashmap.rb
@@ -0,0 +1,11 @@
+class HashMap
+  
+  # Takes a key and produces a hash code with it.
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+    hash_code
+  end
+end
