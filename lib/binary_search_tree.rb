aed0d9ba6fef571a17591ee5e5e119b1db26504f
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-04-23 02:30:21 +0300

    Work on the build tree method in Tree Class

---
+++ lib/binary_search_tree.rb
@@ -0,0 +1,58 @@
+module BinarySearchTree
+  # A node class to help keep track of data
+  class Node
+    include Comparable
+
+    attr_accessor :data, :left, :right
+
+    def initialize(data)
+      @data  = data
+      @left  = nil
+      @right = nil
+    end
+
+    # We compare the nodes using their data attribute.
+    def <=>(other)
+      self.data <=> other.data
+    end   
+  end
+
+  # A Tree class to implement the Binanary search tree operations.
+  class Tree 
+    attr_accessor :root
+
+    # Takes an array when initialized
+    def initialize(array)
+      @array = array
+      @root = build_tree(array)
+    end
+
+    # Constructs a binary search tree from an ordered tree
+    def build_tree(array)
+      recursive_build(array, 0, array.size-1)
+    end
+
+    # A visualizer of a binary search tree.
+    def pretty_print(node=@root, prefix = '', is_left=true)
+      pretty_print(node.right, "#{prefix}#{is_left ? '|   ' : '    '}", false) if node.right
+      puts "#{prefix}#{is_left ? "└── " : "┌── "}#{node.data}"
+      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
+
+    private
+    
+    # Recursive function to construct the BST
+    def recursive_build(arr, start, _end)
+      return nil if start > _end
+
+      mid        = start + (_end - start) / 2
+      root       = Node.new(arr[mid]) # creates a root node
+      root.left  = recursive_build(arr, start, mid-1) # left child
+      root.right = recursive_build(arr, mid+1, _end)  # right child
+      root
+    end
+  end
+end
+
+bst = BinarySearchTree::Tree.new([1, 7, 4, 23, 8, 9, 67, 6345, 324])
+bst.pretty_print
